23129fcc93ea9ecb8e0c37969bc3875e187db402
masi <hoseiny.zp@gmail.com>
2023-05-18 16:13:53 +0330

    edit person

---
+++ ConsoleApp8/models/Person.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp8.models
+{
+    public class Person
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string NationalCode { get; set; }
+        public int mobile { get; set; }
+        public string EmailAddress { get; set; }
+        public string City { get; set; }
+        public int Gender { get; set; }
+        public int Age { get; set; }
+
+
+    }
+}
