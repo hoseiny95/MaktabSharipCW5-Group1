87472d33828e61af5d44366428171946b1cfd80a
masi <hoseiny.zp@gmail.com>
2023-05-18 16:19:51 +0330

    add list

---
+++ ConsoleApp8/Entity/PepoleList.cs
@@ -0,0 +1,14 @@
+﻿using ConsoleApp8.models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp8.Entity
+{
+    internal class PepoleList
+    {
+        List<Person> pepole = new List<Person>();
+    }
+}
